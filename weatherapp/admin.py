ea09c2bfa27554589e07e3592bae90a9bce241de
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-06-09 13:01:24 +0300

    admin page changed

--- weatherapp/admin.py
+++ weatherapp/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import City
 
-admin.site.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    ordering = ("-name",)
+    search_fields = ("name",)
+    list_editable = ("name",)
+    list_per_page = 3
+    
+
+admin.site.register(City, CityAdmin)
